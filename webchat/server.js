cc7160eb131f014692bf050e4c38034b02f4c3d7
jmingzi <472102644@qq.com>
2015-11-08 17:37:10 +0800

    页面布局

--- webchat/server.js
+++ webchat/server.js
@@ -1,6 +1,16 @@
 var express = require('express'),
 	app = express(),
-	server = require('http').createServer(app);
+	server = require('http').createServer(app),
+	    io = require('socket.io').listen(server);
 
-app.use('/', express.static(__dirname + '/html'));
+app.use('/', express.static(__dirname + '/www'));
 server.listen(80);
+console.log('server started');
+//socket部分
+io.on('connection', function(socket) {
+    //接收并处理客户端发送的foo事件
+    socket.on('foo', function(data) {
+        //将消息输出到控制台
+        console.log(data);
+    })
+});
